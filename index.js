53a78e80165aae94b898ae76047f33d08b7fe2dd
Danilo Almeida <danilo00almeida@outlook.com>
2016-11-27 19:28:45 +0000

    Iniciando node

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const http = require('http'),
+	port = process.env.PORT;
+
+const server = http.createServer((req, res)=>{
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port,()=>{
+  console.log(`Server running...`);
+});
+
